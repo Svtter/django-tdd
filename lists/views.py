b77af5a65c551642633a269f26660dfd72f7d297
svtter <svtter@qq.com>
2019-07-06 23:26:10 +0800

    update test

--- lists/views.py
+++ lists/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpRequest
 from django.http import HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 
 
 def home_page(request: HttpRequest):
-    return HttpResponse('<html><title>To-Do lists</title></html>')
+    return render(request, 'home.html')
