2789546be9448996f296feeaff22afa2ff22b70e
svtter <svtter@qq.com>
2019-07-09 10:43:28 +0800

    Separate the URLs. with `path`. weriod about `re_path` not work.

--- lists/tests.py
+++ lists/tests.py
@@ -69,6 +69,7 @@ class ListViewTest(TestCase):
         other_list = List.objects.create()
         correct_list = List.objects.create()
         response = self.client.get(f'/lists/{correct_list.id}')
+        self.assertTemplateUsed(response, 'list.html')
         self.assertEqual(response.context['list'], correct_list)
 
 
