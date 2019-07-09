96ee236fdf0a1f72e182ee7849d6939d5ccc1c8f
svtter <svtter@qq.com>
2019-07-09 14:36:09 +0800

    Update fabfile.py | fix bug

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -11,6 +11,11 @@ def deploy():
     site_folder = f'/home/{env.user}/sites/{env.host}'
     run(f'mkdir -p {site_folder}')
     with cd(site_folder):
+        _get_latest_source()
+        _update_virtualenv()
+        _create_or_update_dotenv()
+        _update_static_files()
+        _update_database()
 
 
 def _get_latest_source():
